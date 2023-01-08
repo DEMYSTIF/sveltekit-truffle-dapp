9b31eb005789fd6476b31f2c6259f6af8d0faa44
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-01-08 20:44:11 +0530

    dynamic_deployerAddress

--- sveltekit/src/lib/instance.js
+++ sveltekit/src/lib/instance.js
@@ -1,6 +1,7 @@
 import { abi, networks } from './Cert.json'
+import { from } from './deployer.json'
 import Web3 from 'web3'
 
 const web3 = new Web3('http://localhost:8545')
 export const mycontract = new web3.eth.Contract(abi, networks['5777'].address)
-export const account = '0x3Ec9c4D6AA38aD7A0De43128C4714F40c850c3Cd'
+export const account = from
--- truffle/migrations/1673066244_cert.js
+++ truffle/migrations/1673066244_cert.js
@@ -1,5 +1,14 @@
+const fs = require("fs");
 const Cert = artifacts.require("Cert");
 
 module.exports = function (deployer) {
-  deployer.deploy(Cert);
+  deployer.deploy(Cert)
+    .then((instance) => {
+      const details = instance.constructor.class_defaults;
+      // console.log(details);
+      fs.writeFileSync('../sveltekit/src/lib/deployer.json', JSON.stringify(details, null, 4));
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
